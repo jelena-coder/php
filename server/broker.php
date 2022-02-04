c55bd5aebc38b25b17af5493dcdcdb4161d87c22
Jelena Slijepcevic <jeca.lelica9983@gmail.com>
2022-02-04 01:10:21 +0100

    inicijalizovan kostur,sredjen izgled,veza sa bazom

---
+++ server/broker.php
@@ -0,0 +1,26 @@
+<?php
+
+class Broker{
+   
+    private $mysqli;
+    private static $broker;
+   
+    private function __construct(){
+        $this->mysqli = new mysqli("localhost","root","","apple");
+        $this->mysqli->set_charset("utf8");
+    }
+
+    public static function getBroker(){
+        if(!isset($broker)){
+            $broker=new Broker();
+        }
+        return $broker;
+    }
+    
+  
+    
+    
+}
+
+
+?>
